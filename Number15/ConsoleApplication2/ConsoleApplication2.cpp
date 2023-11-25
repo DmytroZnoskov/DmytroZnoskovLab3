b8ce18e64100533388c4f7fad55f3655796199da
Dmytro Znoskov <dimasuperpvp@gmail.com>
2023-11-25 18:30:00 +0200

    lab3

---
+++ Number15/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+
+    // Введення користувачем значень a і b
+    std::cout << "Введіть число a: ";
+    std::cin >> a;
+
+    std::cout << "Введіть число b: ";
+    std::cin >> b;
+
+    std::cout << "Числа, які діляться на обидва числа " << a << " і " << b << ":\n";
+
+    int current = 1;
+
+    while (current <= 100) {  // Припустимо, що шукаємо числа в діапазоні від 1 до 100.
+        if (current % a == 0 && current % b == 0) {
+            std::cout << current << " ";
+        }
+
+        ++current;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ Number9/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+    
+
+bool is_prime(int num) {
+    if (num < 2) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int m, n;
+
+    // Введення користувачем значень m і n
+    std::cout << "Введіть початкове число m: ";
+    std::cin >> m;
+
+    std::cout << "Введіть кінцеве число n: ";
+    std::cin >> n;
+
+    std::cout << "Прості числа в діапазоні [" << m << ", " << n << "]:\n";
+
+    int current = m;
+
+    while (current <= n) {
+        if (is_prime(current)) {
+            std::cout << current << " ";
+        }
+
+        ++current;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
